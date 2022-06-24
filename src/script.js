175d8952a975a671ab33044ef81512b4a3e9e817
sospeterbrian029@gmail.com <sospeterbrian029@gmail.com>
2022-06-24 12:34:04 +0300

    adding the innert HTML

--- src/script.js
+++ src/script.js
@@ -8,7 +8,32 @@ getData()
 async function getData(){
     const res = await fetch('https://randomuser.me/api?results=50')
     //call res.json to get our data 
-    const data = await res.json()
+    const { results } = await res.json()
      //testing our data
-    console.log(data)
+    //console.log(data)
+
+    //clear results
+     result.innerHTML = ''
+
+     results.forEach(user => {
+         //testing our users
+         //console.log(user)
+         const li = document.createElement('li')
+         //pushing li
+         listItems.push(li)
+         //add innerHTML
+        li.innerHTML = `
+         <img src="${user.picture.large}" alt="${user.name.first}">
+         <div class="user-info">
+         <h4>${user.name.first} ${user.name.last}</h4>
+         <p>${user.location.city}, ${user.location.country}</p>
+         </div>
+         `
+
+         //putting to the HTML
+         result.appendChild(li)
+     });
+
+
 }
+
